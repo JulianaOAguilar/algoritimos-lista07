097528c08a48d39496418f77e1b89c358046057c
Juliana Aguilar <161730069+JulianaOAguilar@users.noreply.github.com>
2025-05-16 08:51:49 -0300

    Update lista7exercicio06.cpp

--- lista7exercicio06.cpp
+++ lista7exercicio06.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#define LEN 50
+#define LEN 100
 
 int main()
 {
 	/* 6. Preencher automaticamente um vetor com todos os
-	números pares entre 1 e 100 e depois exibir os números
-	para o usuário.*/
+	nÃºmeros pares entre 1 e 100 e depois exibir os nÃºmeros
+	para o usuÃ¡rio.*/
 	 
 	int v[LEN];
 	int len = 0;
